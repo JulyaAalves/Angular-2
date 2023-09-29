b7d478bc14fca92e7cac58ded671992cbde61791
JULYA ALVES CORDEIRO DE MACEDO <0077112@ifmg.edu.br>
2023-09-29 15:30:31 -0300

    Busca Cep Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BuscaCepService } from './service/busca-cep.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'BuscaCep';
+  cep: string = '';
+  cepData: any = {};
+
+  constructor(private cepService: BuscaCepService) { }
+  pesquisarCep() {
+    this.cepService.getCep(this.cep).subscribe((data: any)=> {
+      this.cepData = data;
+      console.log(this.cepData);
+    }
+    )
+  }
+
+
 }
